d892cc9935f0dd64b291bac9bcd8de3c2081d0ca
Raminderpreet90 <kaurr90@student.douglascollege.ca>
2019-11-23 19:39:53 -0800

    WebConfig.java file is added with all Beans

---
+++ src/main/java/com/config/WebConfig.java
@@ -0,0 +1,58 @@
+package com.config;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jndi.JndiTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+@Configuration 
+@EnableWebMvc
+@ComponentScan(basePackages = { "com" })
+
+public class WebConfig implements WebMvcConfigurer{
+	@Autowired
+	 DataSource dataSource;
+	
+	@Bean
+	 public NamedParameterJdbcTemplate geNamedParameterJdbcTemplate(){
+	  return new NamedParameterJdbcTemplate(dataSource);
+	 }
+	
+	@Override
+	 public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	  registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	 }
+	@Bean
+	public InternalResourceViewResolver resolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);      
+		resolver.setPrefix("/WEB-INF/views/");        
+			resolver.setSuffix(".jsp");        
+			return resolver;   
+			}
+	
+	
+	@Bean
+	DriverManagerDataSource getDataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/royalcarrent");
+		ds.setUsername("root");
+		ds.setPassword("");
+
+		return ds;
+	}
+
+}
