f15a78514b975d2e93c3c9902cbb71854a1366c4
Raminderpreet90 <kaurr90@student.douglascollege.ca>
2019-12-01 14:26:12 -0800

    BookCar class added under model

---
+++ src/main/java/com/model/BookCar.java
@@ -0,0 +1,28 @@
+package com.model;
+
+public class BookCar {
+
+	private String Model;
+	private String color;
+	private String carRent;
+	
+	public String getModel() {
+		return Model;
+	}
+	public void setModel(String model) {
+		Model = model;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getCarRent() {
+		return carRent;
+	}
+	public void setCarRent(String carRent) {
+		this.carRent = carRent;
+	}
+	
+}
